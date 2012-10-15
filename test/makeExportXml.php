1e9c00a2f89813b55c8af9f096ab8d1366e2e32c
ragnigk <ralf.biermann@urban-media.com>
2012-10-15 21:42:31 +0200

    a201210152142

--- test/makeExportXml.php
+++ test/makeExportXml.php
@@ -1,43 +1,112 @@
 <?php
+  // XML to Array
+  function xml2ary(&$string) {
+      $parser = xml_parser_create();
+      xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+      xml_parse_into_struct($parser, $string, $vals, $index);
+      xml_parser_free($parser);
+
+      $mnary=array();
+      $ary=&$mnary;
+      foreach ($vals as $r) {
+          $t=$r['tag'];
+          if ($r['type']=='open') {
+              if (isset($ary[$t])) {
+                  if (isset($ary[$t][0])) $ary[$t][]=array(); else $ary[$t]=array($ary[$t], array());
+                  $cv=&$ary[$t][count($ary[$t])-1];
+              } else $cv=&$ary[$t];
+              if (isset($r['attributes'])) {foreach ($r['attributes'] as $k=>$v) $cv['_a'][$k]=$v;}
+              $cv['_c']=array();
+              $cv['_c']['_p']=&$ary;
+              $ary=&$cv['_c'];
+
+          } elseif ($r['type']=='complete') {
+              if (isset($ary[$t])) { // same as open
+                  if (isset($ary[$t][0])) $ary[$t][]=array(); else $ary[$t]=array($ary[$t], array());
+                  $cv=&$ary[$t][count($ary[$t])-1];
+              } else $cv=&$ary[$t];
+              if (isset($r['attributes'])) {foreach ($r['attributes'] as $k=>$v) $cv['_a'][$k]=$v;}
+              $cv['_v']=(isset($r['value']) ? $r['value'] : '');
+
+          } elseif ($r['type']=='close') {
+              $ary=&$ary['_p'];
+          }
+      }
+
+      _del_p($mnary);
+      return $mnary;
+  }
+  // _Internal: Remove recursion in result array
+  function _del_p(&$ary) {
+      foreach ($ary as $k=>$v) {
+          if ($k==='_p') unset($ary[$k]);
+          elseif (is_array($ary[$k])) _del_p($ary[$k]);
+      }
+  }
+
+
   $traueranzeigen = array();
-  foreach (glob("*.pdf") as $filename) {
+  $old = getcwd();
+  $quelle = shell_exec('ls *.tar.gz');
+  shell_exec("tar xfvz $quelle");
+  list($d,$rest) = explode("_",$quelle);
+  chdir($d);
+  foreach (glob("2*.pdf") as $filename) {
       shell_exec("pdftohtml -xml $filename");
+      unlink($filename);
   }
   foreach (glob("*.xml") as $filename) {
       $tr = array();
       list($datum,$sap) = explode('__',basename($filename,'.xml'));
       $tr['datum'] = $datum;
       $tr['sap'] = $sap;
-      $a = json_decode(json_encode((array) simplexml_load_file($filename)),1);
-      foreach($a as $page){
-          //var_dump($page);
-          $tr['hoehe'] = $page["@attributes"]["height"];
-          $tr['breite'] = $page["@attributes"]["width"];
+      //$a = json_decode(json_encode((array) simplexml_load_file($filename)),1);
+      $a = xml2ary(file_get_contents($filename));
+      foreach($a["pdf2xml"]["_c"] as $page){
+          $tr['hoehe'] = $page["_a"]["height"];
+          $tr['breite'] = $page["_a"]["width"];
           $size = 0;
           $id = 0;
-          foreach($page["fontspec"] as $fc){
-              if((int)$fc["@attributes"]["size"] > $size){
-                  $size = (int)$fc["@attributes"]["size"];
-                  $id = (int) $fc["@attributes"]["id"];
+          foreach($page["_c"]["fontspec"] as $fc){
+              if((int)$fc["_a"]["size"] > $size){
+                  $size = (int)$fc["_a"]["size"];
+                  $id = (int) $fc["_a"]["id"];
               }
           }
-          $desc = '';
-          $tr['name'] = '';
-          foreach($page["text"] as $te){
-              if(is_array($te)){
-                  $t = array_values($te);
-                  $text = $t[1];
-              } else {
-                  $text =  $te;
+          $desc = array();
+          $trn = '';
+          foreach($page["_c"]["text"] as $te){
+              if(array_key_exists('_c', $te)){
+                  $k = key($te['_c']);
+                  $text = $te['_c'][$k]['_v'];
+              } else if(array_key_exists('_v', $te)){
+                  $text = $te['_v'];
               }
-              if(is_array($te) && (int)$te["@attributes"]["font"] == $id){
-                  $tr['name'] .=  " ".$text;
+              if((int)$te['_a']["font"] == $id){
+                  $trn .=  " ".$text;
               }
-              $desc .= "\n".$text;
+              $desc[(int)$te['_a']["top"]] = $text;
           }
-          $tr['anzeigentext'] = $desc;
+          $tr['name'] = trim($trn);
+          ksort($desc);
+          $tr['anzeigentext'] = implode("\n",$desc);
       }
       $traueranzeigen[] = $tr;
+      unlink($filename);
   }
-  var_dump($traueranzeigen);
+  $xml = new SimpleXMLElement('<traueranzeigen/>');
+  //var_dump($traueranzeigen);
+  foreach($traueranzeigen as $tr){
+      $trauer = $xml->addChild('anzeige');
+      $trauer->addChild('datum',$tr['datum']);
+      $trauer->addChild('sap',$tr['sap']);
+      $trauer->addChild('hoehe',$tr['hoehe']);
+      $trauer->addChild('breite',$tr['breite']);
+      $trauer->addChild('name',$tr['name']);
+      $trauer->addChild('anzeigentext',$tr['anzeigentext']);
+  }
+  $out = $xml->asXML();
+  chdir($old);
+  file_put_contents($d.'.xml',$out);
+  rmdir($d);
 ?>
